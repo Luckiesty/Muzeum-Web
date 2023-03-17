961793157242c3cfb3555e47a1d7d908c85454d9
Pásztor Bence Péter <71604533754@szily.lan>
2023-03-17 09:57:15 +0100

    Webshop hozzáadása a navbar-hoz + termek_lista.sql létrehozva

---
+++ fooldal/termek_lista.sql
@@ -0,0 +1,3 @@
+CREATE TABLE `darkbluemoon`.`termek_tabla` 
+( `id` INT(8) NOT NULL AUTO_INCREMENT , `termek_nev` VARCHAR(255) NOT NULL , `termek_kod` VARCHAR(255) NOT NULL , `termek_kep` TEXT NOT NULL , `termek_ar` DOUBLE(10,2) NOT NULL , PRIMARY KEY (`id`))
+ENGINE = InnoDB;
--- fooldal/webshop.php
+++ fooldal/webshop.php
@@ -1,3 +1,19 @@
+<?php 
+require_once("db_controller.php");
+$db_kezeles = new DBController();
+if(!empty($_GET["muvelet"]))
+{
+	switch($_GET["muvelet"])
+	{
+		case "hozzaadas":
+			if(!empty($_POST["mennyiseg"]))
+			{
+				$termek_Kodbol = $db_kezeles->runQuery();
+			}
+	}
+}
+?>
+
 <!DOCTYPE html>
 <html lang="hu">
 <head>
@@ -8,25 +24,6 @@
     <title>Webshop</title>
 </head>
 <body>
-<?php
-include("db_controller.php");
-$product_array = $conn->query("SELECT * FROM termek_lista ORDER BY id ASC");
-if (!empty($product_array)) { 
-	foreach($product_array as $key=>$value){
-?>
-	<div class="product-item">
-		<form method="post" action="webshop.php?action=add&code=<?php echo $product_array[$key]["code"]; ?>">
-		<div class="product-image"><img src="<?php echo $product_array[$key]["image"]; ?>"></div>
-		<div class="product-tile-footer">
-		<div class="product-title"><?php echo $product_array[$key]["name"]; ?></div>
-		<div class="product-price"><?php echo "$".$product_array[$key]["price"]; ?></div>
-		<div class="cart-action"><input type="text" class="product-quantity" name="quantity" value="1" size="2" /><input type="submit" value="Add to Cart" class="btnAddAction" /></div>
-		</div>
-		</form>
-	</div>
-<?php
-	}
-}
-?>
+
 </body>
 </html>

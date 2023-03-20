888239076083d5ef12a8bf783db150ba94994d6d
Pásztor Bence Péter <71604533754@szily.lan>
2023-03-20 14:38:37 +0100

    Álltalános hibajavításaok

--- webshop.php
+++ webshop.php
@@ -1,5 +1,5 @@
 <?php 
-require_once("db_controller.php");
+require_once("webshop/db_controller.php");
 $db_handle = new DB_Kontroller();
 if(!empty($_GET["muvelet"]))
 {
@@ -59,7 +59,7 @@ if(!empty($_GET["muvelet"]))
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-	<link rel="stylesheet" href="webshop_style.css">
+	<link rel="stylesheet" href="webshop/webshop_style.css">
     <title>Webshop</title>
 </head>
 <body>

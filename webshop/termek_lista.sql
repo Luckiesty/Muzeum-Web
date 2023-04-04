868b85f3f74e20d5e6fd0ff3f0fd428f2715f7a5
Pásztor Bence <sheperd1031@gmail.com>
2023-04-04 18:16:18 +0200

    termek_lista.sql adatok változtatása

--- webshop/termek_lista.sql
+++ webshop/termek_lista.sql
@@ -1,8 +1,8 @@
-CREATE TABLE `darkbluemoon`.`termek_tabla` 
-( `id` INT(8) NOT NULL AUTO_INCREMENT , `termek_nev` VARCHAR(255) NOT NULL , `termek_kod` VARCHAR(255) NOT NULL , `termek_kep` TEXT NOT NULL , `termek_ar` DOUBLE(10,2) NOT NULL , PRIMARY KEY (`id`))
+CREATE TABLE `darkbluemoon`.`tblproduct` 
+( `id` INT(8) NOT NULL AUTO_INCREMENT , `name` VARCHAR(255) NOT NULL , `code` VARCHAR(255) NOT NULL , `image` TEXT NOT NULL , `price` DOUBLE(10,2) NOT NULL , PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
-INSERT INTO `termek_tabla` (`id`, `termek_nev`, `termek_kod`, `termek_kep`, `termek_ar`) 
+INSERT INTO `termek_tabla` (`id`, `name`, `code`, `image`, `price`) 
 VALUES (1, 'El Greco Póló', '761cgh1', 'termek-kepek/El_Greco_polo.jpg', 3500.00), 
 (2, 'Hantai Simon Füzet', 'HE176f', 'termek-kep/Hantai_Etudes_fuzet.jpg', 1000.00), 
 (3, 'Arany Gyerta Szett (3)', 'AGsz668', 'termek-kepek/Arany_Gyerta_szett.jpg', 2750.00);
